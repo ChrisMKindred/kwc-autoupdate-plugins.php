c9306b4e5b8bcd40996c1a8356b748e3ee67f450
Chris Kindred <Chris@kindredwebconsulting.com>
2017-12-01 12:22:19 -0500

    Remove the email on update

--- kwc-autoupdate.php
+++ kwc-autoupdate.php
@@ -11,7 +11,7 @@ License: GPL2
 
 add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'auto_update_theme', '__return_true' );
-add_filter( 'auto_core_update_send_email', '__return_true' );
+add_filter( 'auto_core_update_send_email', '__return_false' );
 
 add_filter( 'auto_update_core', '__return_true' );
 add_filter( 'allow_minor_auto_core_updates', '__return_true' );
